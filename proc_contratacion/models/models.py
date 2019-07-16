cee7fd06e833c97988a082fa362a9f803240e032
gerardosarracino <gerardo.sarracino.s@gmail.com>
2019-07-15 23:09:19 -0700

    backup

--- proc_contratacion/models/models.py
+++ proc_contratacion/models/models.py
@@ -4,64 +4,66 @@ from odoo import models, fields, api
 
 
 class licitacion(models.Model):
-	_name = "proceso.licitacion"
+    _name = "proceso.licitacion"
 
-	name = fields.Many2one('generales.programas_inversion', 'name')
-	objetolicitacion = fields.Text(string="Programa de Inversión", required=True)
-	select = [('1', 'Licitación publica'), ('2', 'Licitación simplificada/Por invitación')]
-	tipolicitacion = fields.Selection(select, string="Tipo de Licitación", default="1", required=True)
-	numerolicitacion = fields.Char(string="Número de Licitación", required=True)
-	convocatoria = fields.Char(string="Convocatoria",required=True)
-	fechaconinv = fields.Date(string="Fecha Con/Inv", required=True)
-	select1 = [('1', 'Estatal'), ('2', 'Nacional'), ('3', 'Internacional')]
-	caracter = fields.Selection(select1, string="Carácter", default="1", required=True)
-	select2 = [('1', 'Federal'), ('1', 'Estatal')]
-	normatividad = fields.Selection(select2, string="Normatividad", default="1", required=True)
-	funcionariopresideactos = fields.Char(string="Funcionario que preside actos", required=True)
-	puesto = fields.Text(string="Puesto", required=True)
-	numerooficio = fields.Char(string="Numero oficio", required=True)
-	fechaoficio = fields.Date(string="Fecha oficio", required=True)
-	oficioinvitacioncontraloria = fields.Char(string="Oficio invitación contraloría", required=True)
-	fechaoficio2 = fields.Date(string="Fecha oficio", required=True)
-	notariopublico = fields.Text(string="Notario publico", required=True)
-	fechalimiteentregabases = fields.Date(string="Fecha Límite para la entrega de Bases", required=True)
-	fecharegistrocompranet = fields.Date(string="Fecha Registro CompraNet", required=True)
-	costobasesdependencia = fields.Float(string="Costo de Bases Dependencia", required=True)
-	costocompranetbanco = fields.Float(string="Costo CompraNET/Banco", required=True)
-	fechaestimadainicio = fields.Date(string="Fecha Estimada de Inicio", required=True)
-	fechaestimadatermino = fields.Date(string="Fecha Estimada de Termino", required=True)
-	plazodias = fields.Integer(string="Plazo de Días", required=True)
-	capitalcontable = fields.Float(string="Capital Contable", required=True)
-	anticipomaterial = fields.Float(string="Anticipo Material %")
-	anticipoinicio = fields.Float(string="Anticipo Inicio %")
-	puntosminimospropuestatecnica = fields.Char(string="Puntos mínimos propuesta técnica")
-	visitafechahora = fields.Datetime(string="Fecha/Hora")
-	visitalugar = fields.Text(string="Lugar")
-	juntafechahora = fields.Datetime(string="Fecha/Hora")
-	juntalugar = fields.Text(string="Lugar")
-	aperturafechahora = fields.Datetime(string="Fecha/Hora")
-	aperturalugar = fields.Text(string="Lugar")
-	fallofechahora = fields.Datetime(string="Fecha/Hora")
-	fallolugar = fields.Text(string="Lugar")
+    name = fields.Many2one('generales.programas_inversion', 'name')
+    objetolicitacion = fields.Text(string="Objeto De La Licitación", required=True)
+    select = [('1', 'Licitación publica'), ('2', 'Licitación simplificada/Por invitación')]
+    tipolicitacion = fields.Selection(select, string="Tipo de Licitación", default="1", required=True)
+    numerolicitacion = fields.Char(string="Número de Licitación", required=True)
+    convocatoria = fields.Char(string="Convocatoria", required=True)
+    fechaconinv = fields.Date(string="Fecha Con/Inv", required=True)
+    select1 = [('1', 'Estatal'), ('2', 'Nacional'), ('3', 'Internacional')]
+    caracter = fields.Selection(select1, string="Carácter", default="1", required=True)
+    select2 = [('1', 'Federal'), ('1', 'Estatal')]
+    normatividad = fields.Selection(select2, string="Normatividad", default="1", required=True)
+    funcionariopresideactos = fields.Char(string="Funcionario que preside actos", required=True)
+    puesto = fields.Text(string="Puesto", required=True)
+    numerooficio = fields.Char(string="Numero oficio", required=True)
+    fechaoficio = fields.Date(string="Fecha oficio", required=True)
+    oficioinvitacioncontraloria = fields.Char(string="Oficio invitación contraloría", required=True)
+    fechaoficio2 = fields.Date(string="Fecha oficio", required=True)
+    notariopublico = fields.Text(string="Notario publico", required=True)
+    fechalimiteentregabases = fields.Date(string="Fecha Límite para la entrega de Bases", required=True)
+    fecharegistrocompranet = fields.Date(string="Fecha Registro CompraNet", required=True)
+    costobasesdependencia = fields.Float(string="Costo de Bases Dependencia", required=True)
+    costocompranetbanco = fields.Float(string="Costo CompraNET/Banco", required=True)
+    fechaestimadainicio = fields.Date(string="Fecha Estimada de Inicio", required=True)
+    fechaestimadatermino = fields.Date(string="Fecha Estimada de Termino", required=True)
+    plazodias = fields.Integer(string="Plazo de Días", required=True)
+    capitalcontable = fields.Float(string="Capital Contable", required=True)
+    anticipomaterial = fields.Float(string="Anticipo Material %")
+    anticipoinicio = fields.Float(string="Anticipo Inicio %")
+    puntosminimospropuestatecnica = fields.Char(string="Puntos mínimos propuesta técnica")
+    visitafechahora = fields.Datetime(string="Fecha/Hora")
+    visitalugar = fields.Text(string="Lugar")
+    juntafechahora = fields.Datetime(string="Fecha/Hora")
+    juntalugar = fields.Text(string="Lugar")
+    aperturafechahora = fields.Datetime(string="Fecha/Hora")
+    aperturalugar = fields.Text(string="Lugar")
+    fallofechahora = fields.Datetime(string="Fecha/Hora")
+    fallolugar = fields.Text(string="Lugar")
+    # relacion = fields.Many2one('proceso.participantes_contratistas', 'contratista_participante' )
 
-	@api.multi
-	def participantes(self):
-		return {
-			'type': 'ir.actions.act_window',
-			'name': 'Licitantes participantes',
-			'res_model': 'proceso.participantes_contratistas',
-			'view_mode': 'tree,form',
-			'view_type': 'form',
-			'target': 'self',
-		}
+    @api.multi
+    def participantes(self):
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Licitantes participantes',
+            'res_model': 'proceso.participantes_contratistas',
+            # 'views': [(participantes_form, 'tree')],
+            'views': [(False, 'tree'), (False, 'form')],
+            # 'view_id': 'participantes_form',
+            'flags': {'action_buttons': True},
+            'target': 'self',
+
+        }
 
 
 class Participantes(models.Model):
-	_name = "proceso.participantes_contratistas"
+    _name = "proceso.participantes_contratistas"
 
-	contratista_participante = fields.Many2one('contratista.contratista', 'name')
-
-
+    contratista_participante = fields.Many2one('contratista.contratista', 'name')
 
 # class proc_contratacion(models.Model):
 #     _name = 'proc_contratacion.proc_contratacion'
